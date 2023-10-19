cf0f30e1e46b7f19a9880d0116998146e5265235
talha195javed <talha195javed@gmail.com>
2023-10-19 17:58:11 +0500

    simple app created and working

--- src/components/Playing.jsx
+++ src/components/Playing.jsx
@@ -1,13 +1,22 @@
-import React, { useContext, useState } from "react";
-import { AppContext } from "../App";
+import React, {useContext, useState} from "react";
+import {AppContext} from "../App";
 
 function Playing() {
+    const {currentChar} = useContext(AppContext);
 
-    const { currentChar } = useContext(AppContext)
+    const playingNowStyle = {
+        color: "black",
+        fontWeight: "Bolder",
+        paddingBottom: "40%",
+        fontFamily: "cursive",
+    };
 
     return (
-        <div>Playing now: <span>{currentChar}</span></div>
-    )
+        <div style={playingNowStyle}>
+            Playing now: <span>{currentChar}</span>
+        </div>
+    );
 }
+
 
 export default Playing
